a69b64418ead3d648af499305329876a48df2f37
Delbrique <valentinbiyong2@gmail.com>
2026-09-24 10:15:56 +0100

    Use accessible CDN invitation link

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
 const DESTINATAIRE_EMAIL = "biyongdelbrique@gmail.com";
 // Config : ton adresse pour recevoir sa réponse
 const ADRESSE_RETOUR_EMAIL = "valentinbiyong2@gmail.com";
-const INVITATION_URL = "https://delbrique.github.io/invitation-interactive/";
+const INVITATION_URL = "https://cdn.jsdelivr.net/gh/Delbrique/invitation-interactive@main/index.html";
 
 const isReplyMode = new URLSearchParams(window.location.search).get("reply") === "1";
 
